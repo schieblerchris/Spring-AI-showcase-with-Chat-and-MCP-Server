a3e300da9b7a850575be4279c67ee43864fdb98e
Christian Schiebler <chrsitian.schiebler@gmail.com>
2025-11-15 15:59:23 +0100

    improved system prompt to show tool calling and add links to person detail pages

--- src/main/java/com/github/sc/apps/saisc/chat/ChatService.java
+++ src/main/java/com/github/sc/apps/saisc/chat/ChatService.java
@@ -14,6 +14,14 @@ import java.util.List;
 @Service
 public class ChatService {
 
+    private static final String SYSTEM_PROMPT = """
+            You are a personal assistant to help finding the best suited person for a given activity.
+            Always try to find the best fitting person by availability and hobby skill level.
+            To reduce biases resolve the person data, like first name, last name, age, gender, etc. at the last possible moment.
+            If you reference any person add a http link to their profile http://localhost:58080/persons/{personId}.
+            Always include a table containing your tool calls with the input parameters in the final response.
+            """;
+
     private final ChatClient chatClient;
     private final Object[] tools;
 
@@ -25,20 +33,9 @@ public class ChatService {
                 .builder(chatMemory)
                 .build();
         chatClient = chatClientBuilder
-                .defaultSystem("Your goal is to help finding persons. Always try to find the best fitting person by availability and hobby skill level. Resolve the person at the last possible moment from id to the real deal.")
+                .defaultSystem(SYSTEM_PROMPT)
                 .defaultAdvisors(chatMemoryAdvisor)
                 .build();
-    }
-
-    public Flux<String> chatStream(String userInput, String chatId) {
-        return chatClient.prompt()
-                .advisors(advisorSpec ->
-                        advisorSpec.param(ChatMemory.CONVERSATION_ID, chatId)
-                )
-                .user(userInput)
-                .tools(tools)
-                .stream()
-                .content();
     }
 
     public Flux<ChatClientResponse> chatStreamDetailed(String userInput, String chatId) {
