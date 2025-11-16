353bf692f83b41f629765be5d79cccab9c2de121
Christian Schiebler <chrsitian.schiebler@gmail.com>
2025-11-16 17:02:45 +0100

    added index view with automatic redirect to chat

---
+++ src/main/java/com/github/sc/apps/saisc/common/view/IndexView.java
@@ -0,0 +1,20 @@
+package com.github.sc.apps.saisc.common.view;
+
+import com.github.sc.apps.saisc.chat.view.ChatView;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.*;
+
+import java.util.Map;
+import java.util.UUID;
+
+
+@PageTitle("Index")
+@Route(value = "", layout = BaseLayout.class)
+public class IndexView extends VerticalLayout implements BeforeEnterObserver {
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        var newId = UUID.randomUUID().toString();
+        event.rerouteTo(ChatView.class, new RouteParameters(Map.of("chatId", newId)));
+    }
+}
